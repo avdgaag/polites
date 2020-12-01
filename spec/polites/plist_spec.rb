a21356279e928eff4af7bd93815cfc19f8e1e659
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2020-12-01 09:47:29 +0100

    Skip plist specs on CI
    Since plutil is a macos-only program, tests relying on it won't work on
    CI.

--- spec/polites/plist_spec.rb
+++ spec/polites/plist_spec.rb
@@ -5,6 +5,7 @@ require 'polites/plist'
 module Polites
   RSpec.describe Plist do
     it 'reads a binary plist file as JSON' do
+      skip 'Not supported on CI' if ENV['CI']
       plist = Plist.new('spec/fixtures/.Ulysses-Group.plist')
       expect(plist.to_h).to include('sheetClusters' => [])
     end
--- spec/polites/settings_spec.rb
+++ spec/polites/settings_spec.rb
@@ -5,13 +5,13 @@ require 'polites/settings'
 module Polites
   RSpec.describe Settings do
     it 'combines plist files from a directory' do
+      skip 'Not supported on CI' if ENV['CI']
       settings = Settings.from_directory('spec/fixtures')
       expect(settings['defaultPathExtensions']).to eql('ulyz')
     end
 
     it 'is frozen by default' do
-      settings = Settings.from_directory('spec/fixtures')
-      expect(settings).to be_frozen
+      expect(Settings.new).to be_frozen
     end
   end
 end
